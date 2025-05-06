2ba5c3b501b8d980f32554772f7b6b5019d6b514
ETec <s.donati@alumno.etec.um.edu.ar>
2025-05-06 08:37:18 -0300

    Cálculo de Valores de la Función

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -16,4 +16,16 @@ fun main() {
 
     // Mostrar la función en formato ax² + bx + c
     println("La función cuadrática es: ${a}x² + ${b}x + ${c}")
+
+    // Etapa 2 
+    fun calcularFuncionCuadratica(x: Double, a: Double, b: Double, c: Double): Double {
+        return a * x * x + b * x + c
+    }
+    // Mostrar tabla de valores para un rango de x
+    println("Tabla de valores:")
+    println("x\tf(x)")
+    for (x in -5..5) {
+        val fx = calcularFuncionCuadratica(x.toDouble(), a, b, c)
+        println("$x\t$fx")
+    }
 }
